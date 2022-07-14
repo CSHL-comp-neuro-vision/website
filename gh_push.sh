c5ed846e435f0ebd139f9270f0eec5408e1d57bd
Dan Birman <danbirman@gmail.com>
2022-07-14 11:23:40 -0400

    Updating shell script

--- gh_push.sh
+++ gh_push.sh
@@ -1,15 +1,20 @@
 # Script to push html doc files to gh pages
+make html
 
 # Repo information
-ORG=dbirman
-REPO=vbl-docs
+ORG=CSHL-comp-neuro-vision
+REPO=website
+
+mkdir ~/temp/
+# Copy the build folder into a temp folder
+cp -R ./build/html/* ~/temp/
 
 # Clone the gh-pages branch to local documentation directory
 git clone -b gh-pages "https://github.com/$ORG/$REPO.git" gh-pages
 cd gh-pages
 
-# Copy everything from output of build into gh-pages branch
-cp -R docs/build/html/* ./
+# Copy everything from temp into gh-pages branch
+cp -R ~/temp/* ./
 
 # Add and commit all changes
 git add -A .
